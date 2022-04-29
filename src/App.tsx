288c95633fa3aeb9b3ce9b479329b30b6cdde919
Daniel Lucas Santos <daniellucas-pms@hotmail.com>
2022-04-29 15:32:18 -0300

    estilos globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
-import styled from "styled-components";
-
-const Title = styled.h1`
-  font-size: 64px;
-  color: #8257e6;
-`
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
     <div>
-      <Title>Hello</Title>
+      <h1>Hello</h1>
+      <GlobalStyle />
     </div>
   );
 }
