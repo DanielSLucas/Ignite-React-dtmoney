22964f08dda6e0e3a6b09e8f60a401265218dc07
Daniel Lucas Santos <daniellucas-pms@hotmail.com>
2022-04-29 15:36:27 -0300

    fontes do google fonts

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -10,7 +10,7 @@ export const GlobalStyle = createGlobalStyle`
 
     --text-title: #363F5F;
     --text-body: #969CB3;
-    
+
     --background: #F0F2F5;
     --shape: #FFFFFF;
   }
@@ -36,7 +36,16 @@ export const GlobalStyle = createGlobalStyle`
   body {
     background: var(---background);
     -webkit-font-smoothing: antialiased;
-  }  
+  }
+
+  body, input, textarea, button {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 600;
+  }
 
   button {
     cursor: pointer;
