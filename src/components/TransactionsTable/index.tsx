7452b283cea166a8d5dfe4f3c711ba861311d6a9
Daniel Lucas Santos <daniellucas-pms@hotmail.com>
2022-05-02 18:28:48 -0300

    usando cliente axios

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,12 +1,12 @@
 import React, { useEffect } from 'react';
+import { api } from '../../services/api';
 
 import { Container } from './styles';
 
 const TransactionsTable: React.FC = () => {
   useEffect(() => {
-    fetch('http://localhost:3000/api/transactions')
-      .then(response => response.json())
-      .then(data => console.log(data))
+    api.get('transactions')
+      .then(response => console.log(response.data));
   },[]);
   
   return (
