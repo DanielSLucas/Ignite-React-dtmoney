956da17042b6e612213b8fed5af93c2df3ab1d1f
Daniel Lucas Santos <daniellucas-pms@hotmail.com>
2022-05-02 14:06:41 -0300

    componente Summary

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -4,14 +4,14 @@ export const GlobalStyle = createGlobalStyle`
   :root {    
     --background: #f0f2f5;
     --red: #E52E4D;
+    --green: #33CC95;
     --blue: #5429CC;
 
     --blue-light: #6933FF;
 
     --text-title: #363F5F;
     --text-body: #969CB3;
-
-    --background: #F0F2F5;
+    
     --shape: #FFFFFF;
   }
   
@@ -34,7 +34,7 @@ export const GlobalStyle = createGlobalStyle`
   }
 
   body {
-    background: var(---background);
+    background: var(--background);
     -webkit-font-smoothing: antialiased;
   }
 
